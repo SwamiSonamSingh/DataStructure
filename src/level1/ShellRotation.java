36fd490d68e150eb44d6fa741d0ae251ba2e89d0
Swami Sonam Singh <swamisonamkumarsingh576@gmail.com>
2022-02-03 19:56:26 +0530

    kkjjl

---
+++ src/level1/ShellRotation.java
@@ -0,0 +1,18 @@
+package level1;
+import java.util.*;
+public class ShellRotation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        int arr[][]=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int shellNo= sc.nextInt();
+        int rotation= sc.nextInt();
+
+    }
+}
