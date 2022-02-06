eef9fd881b97ba75f7068dc59aa469c54fb25e40
Swami Sonam Singh <swamisonamkumarsingh576@gmail.com>
2022-02-06 14:33:39 +0530

    daasf

--- src/level1/ShellRotation.java
+++ src/level1/ShellRotation.java
@@ -13,6 +13,76 @@ public class ShellRotation {
         }
         int shellNo= sc.nextInt();
         int rotation= sc.nextInt();
-
+        int minRow=shellNo-1;
+        int minCol=shellNo-1;
+        int maxRow=row-shellNo;
+        int maxCol=col-shellNo;
+        int size=2*(maxRow-minRow+maxCol-minCol);
+        int resultArr[]=new int[size];
+        int k=0;
+        while(k!=size) {
+            for (int i = minRow, j = minCol; i <= maxRow && k<size; i++) {
+                resultArr[k] = arr[i][j];
+                k++;
+            }
+            for (int i = maxRow, j = minCol + 1; j <= maxCol && k<size; j++) {
+                resultArr[k] = arr[i][j];
+                k++;
+            }
+            for (int i = maxRow - 1, j = maxCol; i >= minRow && k<size; i--) {
+                resultArr[k] = arr[i][j];
+                k++;
+            }
+            for (int i = minRow, j = maxCol - 1; j >minCol && k<size; j--) {
+                resultArr[k] = arr[i][j];
+                k++;
+            }
+        }
+        if(rotation<0){
+            rotation=size+rotation;
+            rotation=rotation%size;
+            int start1 = 0;
+            int end1 = resultArr.length - rotation - 1;
+            int start = end1 + 1;
+            int end = resultArr.length - 1;
+            while (start < end) {
+                int temp = resultArr[start];
+                resultArr[start] = resultArr[end];
+                resultArr[end] = temp;
+                start++;
+                end--;
+            }
+            while (start1 < end1) {
+                int temp = resultArr[start1];
+                resultArr[start1] = resultArr[end1];
+                resultArr[end1] = temp;
+                start1++;
+                end1--;
+            }
+        }
+        if(rotation>0) {
+            rotation=rotation%size;
+            int start1 = 0;
+            int end1 = resultArr.length - rotation - 1;
+            int start = end1 + 1;
+            int end = resultArr.length - 1;
+            while (start < end) {
+                int temp = resultArr[start];
+                resultArr[start] = resultArr[end];
+                resultArr[end] = temp;
+                start++;
+                end--;
+            }
+            while (start1 < end1) {
+                int temp = resultArr[start1];
+                resultArr[start1] = resultArr[end1];
+                resultArr[end1] = temp;
+                start1++;
+                end1--;
+            }
+        }
+        for(int i=resultArr.length-1;i>=0;i--){
+            System.out.println(resultArr[i]);
+        }
     }
 }
