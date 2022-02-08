a64ed7abffa9dbebe97e07e1c04a99f9bc014628
Swami Sonam Singh <swamisonamkumarsingh576@gmail.com>
2022-02-08 22:05:18 +0530

    sadsfsf

---
+++ src/level1/CompressString.java
@@ -0,0 +1,9 @@
+package level1;
+import java.util.*;
+public class CompressString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+
+    }
+}
